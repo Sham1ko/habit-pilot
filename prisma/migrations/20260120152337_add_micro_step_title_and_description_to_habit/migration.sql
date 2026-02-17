aa11a93c6947b442aec9ce7741388c4162a5d3a4
Sham <shamshyrak.zholdasbek@gmail.com>
2026-02-17 15:52:21 +0100

    refactor: remove obsolete migration files and schema definitions

--- prisma/migrations/20260120152337_add_micro_step_title_and_description_to_habit/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `micro_weight_cu` to the `habits` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "habits" ADD COLUMN     "micro_title" TEXT,
-ADD COLUMN     "micro_weight_cu" DECIMAL NOT NULL;
--- prisma/migrations/20260120225443_add_default_weekly_capacity_unit_to_user_model/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "weekly_capacity_cu_default" DECIMAL;
