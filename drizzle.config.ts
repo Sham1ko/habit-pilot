2081f6b866d635734a268c3214e13eb9a3750a92
Sham <shamshyrak.zholdasbek@gmail.com>
2026-02-17 19:40:01 +0100

    chore: update pnpm-workspace.yaml to include better-sqlite3, workerd, and prisma as only built dependencies

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,15 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-	schema: "./lib/db/schema.ts",
-	out: "./drizzle",
-	dialect: "postgresql",
-	dbCredentials: {
-		url: process.env.DIRECT_URL ?? "",
-	},
+  schema: "./lib/db/schema.ts",
+  out: "./drizzle",
+
+  dialect: "sqlite",
+  driver: "d1-http",
+
+  dbCredentials: {
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+    databaseId: process.env.CLOUDFLARE_D1_DATABASE_ID!,
+    token: process.env.CLOUDFLARE_D1_TOKEN!,
+  },
 });
