3c6a35fe6ae7e17200fd0e658f499b8dde878d1c
Sham <shamshyrak.zholdasbek@gmail.com>
2026-02-17 20:17:46 +0100

    chore: clean up configuration files and update dependencies for D1 integration

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -6,15 +6,7 @@ function createDb() {
   const databaseId = process.env.CLOUDFLARE_D1_DATABASE_ID;
   const token = process.env.CLOUDFLARE_D1_TOKEN;
 
-  // Use remote D1 via HTTP API when all credentials are provided
-  if (accountId && databaseId && token) {
-    return drizzle({ accountId, databaseId, token, schema });
-  }
+  return drizzle({ accountId, databaseId, token, schema });
 }
 
-const globalForDb = globalThis as { _db?: ReturnType<typeof createDb> };
-if (!globalForDb._db) {
-  globalForDb._db = createDb();
-}
-
-export const db = globalForDb._db;
+export const db = createDb();
