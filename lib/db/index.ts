a741eda75777157bfb59f32cbe31e4fc0a2e89a5
Sham <shamshyrak.zholdasbek@gmail.com>
2026-02-18 02:32:06 +0100

    chore: format code in getDb function for consistency

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,6 +3,6 @@ import { drizzle } from "drizzle-orm/d1";
 import * as schema from "./schema";
 
 export function getDb() {
-  const { env } = getCloudflareContext<{ env: { DB: D1Database } }>();
-  return drizzle(env.DB, { schema });
+    const { env } = getCloudflareContext<{ env: { DB: D1Database } }>();
+    return drizzle(env.DB, { schema });
 }
